5a27f7e81cc786436ecc3522a42d73668c6c78cb
dfamrln <mhdffa1@gmail.com>
2026-04-09 21:18:38 +0700

    Perbaikan nama file

---
+++ script.js
@@ -0,0 +1,15 @@
+var video = document.getElementById("video");
+var audio = document.getElementById("audio");
+var gambar = document.getElementById("gambar");
+
+video.addEventListener("click", function () {
+    alert("Ini Video Scene dari Arcane");
+});
+
+audio.addEventListener("click", function () {
+    alert("Ini Lagu Come Play dari Arcane");
+});
+
+gambar.addEventListener("click", function () {
+    alert("Ini Karakter dari Series Arcane");
+});
